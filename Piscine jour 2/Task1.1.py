ea007d249e16f367bbe8c9cc404c998d5fd21e10
Gonzalez Lucas <144592295+LucasGonz27@users.noreply.github.com>
2025-09-12 15:24:31 +0200

    Refactor calculations and remove commented code

--- Piscine jour 2/Task1.1.py
+++ Piscine jour 2/Task1.1.py
@@ -1,13 +1,11 @@
-#Task 1 :
+resultat = 1 + 5
+print(resultat) # Affiche 2
 
-#resultat = 1 + 5
-#print(resultat) # Affiche 2
+resultat = 30 + 12
+print(resultat) # Affiche 42
 
-#resultat = 30 + 12
-#print(resultat) # Affiche 42
+resultat = 777 + (-735)
+print(resultat) # Affiche 42
 
-#resultat = 777 + (-735)
-#print(resultat) # Affiche 42
-
-#resultat = 1 + 2 + 3 + 5 + 7 + 11 + 13
-#print(resultat) # Affiche 42
+resultat = 1 + 2 + 3 + 5 + 7 + 11 + 13
+print(resultat) # Affiche 42
