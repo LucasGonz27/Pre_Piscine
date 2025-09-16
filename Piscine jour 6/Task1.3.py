db7989ecc57d002e0e60dcbf2a5b88e850360442
Lucas <lucas.gonz2702@gmail.com>
2025-09-16 10:11:21 +0200

    modif

--- Piscine jour 6/Task1.3.py
+++ Piscine jour 6/Task1.3.py
@@ -1,13 +1,28 @@
-def bread() :
-    print(" <===========> ")
-def lettuce() :
-    print(" ~~~~~~~~~~~~ ")
-def tomato() :
-    print(" ooooooooo ")
-def ham() :
-    print(" ======== ")
+def bread():
+	print(" <===========> ")
 
-def hamburger():
-    print(bread(), lettuce(),ham(), tomato(), ham(),bread())
-    
-hamburger()
+def lettuce():
+	print(" ~~~~~~~~~~~~ ")
+
+def tomato():
+	print(" ooooooooo ")
+
+def ham():
+	print(" ======== ")
+
+listeIngredient = [
+	[lettuce, tomato, ham, ham],
+	[lettuce, ham, tomato, ham],
+	[tomato, lettuce, ham, ham],
+	[ham, ham, lettuce, tomato],
+]
+
+for i in range(4):
+	print(f"\nBurger {i + 1}:")
+	bread()
+	ingredients = listeIngredient[i % len(listeIngredient)]
+	for ingredient in ingredients:
+		ingredient()
+	bread()
+
+
